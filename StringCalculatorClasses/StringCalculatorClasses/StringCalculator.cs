c577e93569a32b803705472bdf7a54217665261c
Ola Alataiwi <olaabilal1@gmail.com>
2022-11-28 19:28:47 +0200

    Step1 Completed

---
+++ StringCalculatorClasses/StringCalculatorClasses/StringCalculator.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace StringCalculatorClasses
+{
+    public class StringCalculator
+    {
+        public int Add(String numbers)
+        {
+            if (numbers == null)
+                throw new ArgumentNullException("Invalid numbers!");
+            if (string.IsNullOrWhiteSpace(numbers))
+                return 0;
+            else
+            {
+                int sum = 0;
+                var numbersList = numbers.Split(',');
+                if (numbersList.Length > 3)
+                    throw new ArgumentException("Must be 3 numbers or less");
+                foreach (var number in numbersList)
+                    sum += int.Parse(number);
+                return sum;
+            }
+        }
+    }
+}
---
+++ StringCalculatorClasses/StringCalculatorTests/StringCalculatorTests.cs
@@ -0,0 +1,51 @@
+﻿using StringCalculatorClasses;
+using System;
+using Xunit;
+
+namespace StringCalculatorTests
+{
+    public class StringCalculatorTests
+    {
+        private StringCalculator _stringCalculator;
+        public StringCalculatorTests()
+        {
+            _stringCalculator = new StringCalculator();
+        }
+
+        [Fact]
+        public void ShouldThrowArgumentNullExceptionWhenInputIsNull()
+        {
+            Assert.Throws<ArgumentNullException>(() => _stringCalculator.Add(null));
+        }
+
+        [Theory]
+        [InlineData("", 0)]
+        [InlineData(" ", 0)]
+        [InlineData("\n", 0)]
+        public void ShouldReturnZeroIfInputIsWhiteSpaceOrEmpty(String input, int expected)
+        {
+            int actual = _stringCalculator.Add(input);
+            Assert.Equal(actual, expected);
+        }
+
+        [Theory]
+        [InlineData("0", 0)]
+        [InlineData("0,1", 1)]
+        [InlineData("0,1,2", 3)]
+        [InlineData("115", 115)]
+        public void ShouldReturnSumOfNumbersIfInputContainsThreeOrLessNumbers(string input, int expected)
+        {
+            int actual = _stringCalculator.Add(input);
+            Assert.Equal(actual, expected);
+        }
+
+        [Theory]
+        [InlineData("1,2,3,4")]
+        [InlineData("1,2,3,4,7,2")]
+        public void ShouldThrowArgumentExceptionWhenInputContainsMoreThanThreeNumbers(String input)
+        {
+            Assert.Throws<ArgumentException>(() => _stringCalculator.Add(input));
+        }
+
+    }
+}
